2112ceaf98bb96ca2e00953ead266ee6dab5981d
unknown <gg@.(none)>
2011-09-24 16:21:04 +0200

    Initial commit - testing git

---
+++ ezinfo.php
@@ -0,0 +1,26 @@
+<?php
+/**
+* Description
+*
+* @package
+*
+* @version $Id$;
+*
+* @author
+* @copyright
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License
+*/
+
+class ezcontentstagingModuleInfo
+{
+    function info()
+    {
+        return array( 'Name' => "eZ Publish Content Staging extension",
+                      'Version' => "0.1",
+                      'Copyright' => "Copyright (C) 2011 Alti",
+                      'License' => "GNU General Public License v2.0"
+                     );
+    }
+}
+
+?>
