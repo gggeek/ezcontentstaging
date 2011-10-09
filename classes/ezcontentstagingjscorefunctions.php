03ac5ab208caaebc388caa835a8e5ff68ee83449
Gaetano Giunta <gg@ez.no>
2011-10-09 14:54:45 +0200

    Add skeleton for ezjscore-based node syncing

---
+++ classes/ezcontentstagingjscorefunctions.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @package ezcontentstaging
+ *
+ * @version $Id$;
+ *
+ * @author
+ * @copyright
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ *
+ */
+
+class eZContentStagingJSCoreFunctions
+{
+    static function syncnode()
+    {
+
+    }
+}
+?>
---
+++ settings/ezjscore.ini.append.php
@@ -0,0 +1,12 @@
+<?php /*
+
+[ezjscServer]
+# instead of relying on this, we check inside the php code for access to contentstaging/sync etc...
+#FunctionList[]=ezcontentstaging_syncnode
+
+[ezjscServer_ezcontentstaging> as class name if not set
+Class=eZContentStagingJSCoreFunctions
+#PermissionPrFunction=enabled
+#Functions[]=ezcontentstaging
+
+*/ ?>
