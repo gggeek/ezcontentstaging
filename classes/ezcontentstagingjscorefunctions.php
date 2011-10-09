7a95c7f607df95157262ae10e4bbb0491a24e8ed
Gaetano Giunta <gg@ez.no>
2011-10-09 17:38:04 +0200

    Advance on ezjscore syncing

--- classes/ezcontentstagingjscorefunctions.php
+++ classes/ezcontentstagingjscorefunctions.php
@@ -10,11 +10,52 @@
  *
  */
 
+
 class eZContentStagingJSCoreFunctions
 {
-    static function syncnode()
+    /**
+    * @param array $args ( 0 => node_id,  1 => target_id )
+    * @return array
+    *
+    * @todo add i18n of returned messages
+    */
+    static function syncnode( $args )
     {
-
+        if ( count( $args ) < 1 )
+        {
+            return array( 'errors' => array( 'Wrong parameter count' ) );
+        }
+        if ( count( $args ) < 2 )
+        {
+            $events = eZContentStagingEvent::fetchByNode( $args[0] );
+        }
+        else
+        {
+            $events = eZContentStagingEvent::fetchByNode( $args[0], null, $args[1] );
+        }
+        if ( count( $events ) )
+        {
+            $syncErrors = array();
+            $syncResults = array();
+            foreach( eZContentStagingEvent::syncEvents( $events ) as $id => $resultCode )
+            {
+                $event = $tosync[$id];
+                if ( $resultCode !== 0 )
+                {
+                    $syncErrors[] = "Event $id: failure ($resultCode)";
+                }
+                else
+                {
+                    $syncResults[] = "Event $id: success";
+                }
+            }
+            return array( 'errors' => $syncErrors, 'results' => $syncResults );
+        }
+        else
+        {
+            return array( 'errors' => array( 'No events found for node ' . (int)$args[0] ) );
+        }
     }
 }
+
 ?>
