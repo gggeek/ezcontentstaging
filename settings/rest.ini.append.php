99a6e1939cf80b601bcb610823c35c335718598c
Damien Pobel <dp@ez.no>
2011-11-02 15:17:27 +0100

    Implemented a filter to auto auth a user on content staging rest actions

--- settings/rest.ini.append.php
+++ settings/rest.ini.append.php
@@ -1,19 +1,14 @@
 <?php /*
 
-[Authentication]
-# disabled all authentication and force admin user for now
-# should be implemented perhaps in the provider
-# or with a custom AuthenticationStyle
-RequireAuthentication=disabled
-RequireHTTPS=disabled
-DefaultUserID=14
-
 [ApiProvider]
 ProviderClass[contentstaging]=contentStagingRestApiProvider
 
 [RouteSettings]
-# Skip (auth) filter for every action in 'myController' which is of API version 2
-SkipFilter[]=contentStagingRestApiController_*
+# Skip (auth) filter for every action in 'myController' which is of API version 1
+SkipFilter[]=contentStagingRestContentController_*
+
+[RequestFilters]
+Filters[]=contentStagingAutoAuthFilter
 
 [contentStagingRestContentController_remove_CacheSettings]
 ApplicationCache=disabled
