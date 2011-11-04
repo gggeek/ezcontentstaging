cbe442a4c63c6131d1202b7aaf6106d820303602
Gaetano Giunta <gg@ez.no>
2011-11-04 15:10:37 +0100

    Add (for ez 4.6 and up) a contenstaging tab in content view

---
+++ settings/admininterface.ini.append.php
@@ -0,0 +1,13 @@
+<?php /*
+
+[WindowControlsSettings]
+AdditionalTabs[]=contentstaging
+
+[AdditionalTab_contentstaging]
+Title=
+Description=Content staging
+NavigationPartName=ezcontentnavigationpart
+HeaderTemplate=content/contentstaging_header.tpl
+Template=content/contentstaging.tpl
+
+*/ ?>
