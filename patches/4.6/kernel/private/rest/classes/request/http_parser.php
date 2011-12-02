a1b56eddafc6087aa064d598a4dff96648d63bee
Gaetano Giunta <gg@ez.no>
2011-12-02 15:34:28 +0100

    Revert (by hand...) unwanted delete of some files by 1st jlm merge

---
+++ patches/4.6/kernel/private/rest/classes/request/http_parser.php
@@ -0,0 +1,221 @@
+<?php
+/**
+ * File containing the ezpRestHttpRequestParser class
+ *
+ * @copyright Copyright (C) 1999-2011 eZ Systems AS. All rights reserved.
+ * @license http://ez.no/eZPublish/Licenses/eZ-Business-Use-License-Agreement-eZ-BUL-Version-2.0 eZ Business Use License Agreement Version 2.0
+ * @version 4.6.0
+ * @package kernel
+ */
+
+/**
+ * Custom request parser which creates instances of ezpRestRequest.
+ *
+ * The main difference is that GET and POST data is protected from potential
+ * cookie pollution. And each category of variable has its own silo, to prevent
+ * one from overwriting another.
+ */
+class ezpRestHttpRequestParser extends ezcMvcHttpRequestParser
+{
+    /**
+     * @var ezpRestRequest
+     */
+    protected $request;
+
+    /**
+     * Overload createRequestObject() to make sure ezpRestRequest is created.
+     *
+     * @return ezpRestRequest
+     */
+    protected function createRequestObject()
+    {
+        return new ezpRestRequest();
+    }
+
+    /**
+     * Overloads processVariables() to instead get ezpRest specific variables
+     *
+     * Note: ->variables is set with ezpRest specific variables instead of raw $_REQUEST.
+     *
+     * @return void
+     */
+    protected function processVariables()
+    {
+        $this->request->variables = $this->fillVariables();
+        $this->request->contentVariables = $this->fillContentVariables();
+        $this->request->get = $_GET;
+        $this->request->post = $_POST;
+    }
+
+    /**
+     * Overloads parent::processStandardHeaders() to also call processEncryption()
+     *
+     * @return void
+     */
+    protected function processStandardHeaders()
+    {
+        $this->processEncryption();
+        parent::processStandardHeaders( );
+        $this->processProtocolOverride();
+    }
+
+    /**
+     * Sets the isEncrypted flag if HTTPS is on.
+     *
+     * @return void
+     */
+    protected function processEncryption()
+    {
+        if ( !empty( $_SERVER['HTTPS'] ) )
+            $this->request->isEncrypted = true;
+    }
+
+
+    /**
+     * Overloads processBody() to add support for body on POST and PUT
+     * NB: this is called before processProtocolOverride(), so it uses the true protocol by default
+     */
+    protected function processBody()
+    {
+        $req = $this->request;
+
+        if ( $req->protocol === 'http-put' ||  $req->protocol === 'http-post' )
+        {
+            $req->body = file_get_contents( "php://input" );
+
+            if ( isset( $headers['CONTENT_TYPE'] ) &&  strlen( $req->body ) > 0 )
+            {
+                switch( $headers['CONTENT_TYPE'] )
+                {
+                    case 'application/json':
+                    case 'json':
+                        $variables = json_decode( $this->request->body, true );
+                        if ( is_array( $variables ) )
+                        {
+                            $this->request->inputVariables = $variables;
+                        }
+                        else
+                        {
+                            /// @todo log warning
+                            $this->request->inputVariables = null;
+                        }
+                        break;
+                    case 'application/x-www-form-urlencoded':
+                        if ( $req->protocol === 'http-put' )
+                        {
+                            $variables = array();
+                            parse_str( $this->request->body, $variables );
+                            $this->request->inputVariables = $variables;
+                        }
+                        else
+                        {
+                            $this->request->inputVariables = $_POST;
+                        }
+                        break;
+                    default:
+                        /// @todo log warning
+                        $this->request->inputVariables = null;
+                }
+            }
+        }
+        $this->request->inputVariables = null;
+    }
+
+    /**
+     * Extract variables to be used internally from GET
+     * @return array
+     */
+    protected function fillVariables()
+    {
+        $variables = array();
+        $internalVariables = array( 'ResponseGroups' ); // Expected variables
+
+        foreach( $internalVariables as $internalVariable )
+        {
+            if( isset( $_GET[$internalVariable] ) )
+            {
+                // Extract and organize variables as expected
+                switch( $internalVariable )
+                {
+                    case 'ResponseGroups':
+                        $variables[$internalVariable] = explode( ',', $_GET[$internalVariable] );
+                        break;
+
+                    default:
+                        $variables[$internalVariable] = $_GET[$internalVariable];
+                }
+
+                unset( $_GET[$internalVariable] );
+            }
+            else
+            {
+                switch( $internalVariable )
+                {
+                    case 'ResponseGroups':
+                        $variables[$internalVariable] = array();
+                        break;
+
+                    default:
+                        $variables[$internalVariable] = null;
+                }
+            }
+        }
+
+        return $variables;
+    }
+
+    /**
+     * Extract variables related to content from GET
+     *
+     * @return array
+     */
+    protected function fillContentVariables()
+    {
+        $contentVariables = array();
+        $expectedVariables = array( 'Translation', 'OutputFormat' );
+
+        foreach( $expectedVariables as $variable )
+        {
+            if( isset( $_GET[$variable] ) )
+            {
+                // Extract and organize variables as expected
+                switch( $variable )
+                {
+                    case 'Translation': // @TODO => Make some control on the locale provided
+                    default:
+                        $contentVariables[$variable] = $_GET[$variable];
+                }
+
+                unset( $_GET[$variable] );
+            }
+            else
+            {
+                $contentVariables[$variable] = null;
+            }
+        }
+
+        return $contentVariables;
+    }
+
+    /**
+     * Adds support for using POST for PUT and DELETE for legacy browsers that does not support these.
+     *
+     * If a post param "_method" is set to either PUT or DELETE, then ->protocol is changed to that.
+     * ( original protocol is kept on ->originalProtocol param  )
+     * Post is used as this is only meant for forms in legacy browsers.
+     */
+    protected function processProtocolOverride()
+    {
+        $req = $this->request;
+        $req->originalProtocol = $req->protocol;
+
+        if ( $req->protocol === 'http-post' && isset( $req->post['_method'] ) )
+        {
+            $method = strtolower( $req->post['_method'] );
+            if ( $method  === 'put' || $method === 'delete' )
+                $req->protocol = "http-{$method}";
+
+            unset( $req->post['_method'] );
+        }
+    }
+}
---
+++ patches/4.6/kernel/private/rest/classes/request/rest_request.php
@@ -0,0 +1,212 @@
+<?php
+/**
+ * File containing the ezpRestRequest class
+ *
+ * @copyright Copyright (C) 1999-2011 eZ Systems AS. All rights reserved.
+ * @license http://ez.no/eZPublish/Licenses/eZ-Business-Use-License-Agreement-eZ-BUL-Version-2.0 eZ Business Use License Agreement Version 2.0
+ * @version 4.6.0
+ * @package rest
+ */
+
+/**
+ * Class mimicking ezcMvcRequest with distinct containers for GET and POST variables.
+ *
+ * The current implementation is a tentative implementation, for long term
+ * usage, we are likely to use dedicated structs such as for cookie. This in
+ * addition or alternatively to a more selective parser, which could cherry pick
+ * variables depending on request type, context and so forth.
+ */
+class ezpRestRequest extends ezcMvcRequest
+{
+    /**
+     * GET variables
+     *
+     * @var array
+     */
+    public $get;
+
+    /**
+     * POST variables
+     *
+     * @var array
+     */
+    public $post;
+
+    /**
+     * Original request method
+     *
+     * @var string
+     */
+    public $originalProtocol;
+
+    /**
+     * PUT & POST variables
+     *
+     * @var array
+     */
+    public $inputVariables;
+
+    /**
+     * Variables related to content, extracted from GET
+     *
+     * @var array
+     */
+    public $contentVariables;
+
+    /**
+     * Signifies whether the request was made over an encrypted connection.
+     *
+     * @var bool
+     */
+    public $isEncrypted;
+
+    /**
+     * Constructs a new ezpRestRequest.
+     *
+     * @param DateTime $date
+     * @param string $protocol
+     * @param string $host
+     * @param string $uri
+     * @param string $requestId
+     * @param string $referrer
+     * @param array $variables Containing request variables set by the router
+     * @param array $get The GET variables which are available in the request
+     * @param array $post The POST variables that are available in the request
+     * @param array $contentVariables GET variables related to eZ Publish content
+     * @param bool $isEncrypted Is the request made over an encrypted connection
+     * @param string $body
+     * @param array(ezcMvcRequestFile) $files
+     * @param ezcMvcRequestAccept $accept
+     * @param ezcMvcRequestUserAgent $agent
+     * @param ezcMvcRequestAuthentication $authentication
+     * @param ezcMvcRawRequest $raw
+     * @param array(ezcMvcRequestCookie) $cookies
+     * @param bool $isFatal
+     * @param string|null $originalProtocol Uses $protocol if null
+     * @param array $inputVariables The PUT & DELETE input variables that are available in the request
+     * @return ezpRestRequest
+     *
+     */
+    public function __construct( $date = null, $protocol = '',
+        $host = '', $uri = '', $requestId = '', $referrer = '',
+        $variables = array(), $get = array(), $post = array(),
+        $contentVariables = array(), $isEncrypted = false, $body = '',
+        $files = null, $accept = null, $agent = null, $authentication = null,
+        $raw = null, $cookies = array(), $isFatal = false, $originalProtocol = null,
+        $inputVariables = array() )
+    {
+        $this->date = $date;
+        $this->protocol = $protocol;
+        $this->host = $host;
+        $this->uri = $uri;
+        $this->requestId = $requestId;
+        $this->referrer = $referrer;
+        $this->variables = $variables;
+        $this->get = $get;
+        $this->post = $post;
+        $this->contentVariables = $contentVariables;
+        $this->isEncrypted = $isEncrypted;
+        $this->body = $body;
+        $this->files = $files;
+        $this->accept = $accept;
+        $this->agent = $agent;
+        $this->authentication = $authentication;
+        $this->raw = $raw;
+        $this->cookies = $cookies;
+        $this->originalProtocol = ( $originalProtocol === null ? $protocol : $originalProtocol );
+        $this->inputVariables = $inputVariables;
+    }
+
+    /**
+     * Returns a new instance of this class with the data specified by $array.
+     *
+     * $array contains all the data members of this class in the form:
+     * array('member_name'=>value).
+     *
+     * __set_state makes this class exportable with var_export.
+     * var_export() generates code, that calls this method when it
+     * is parsed with PHP.
+     *
+     * @param array(string=>mixed) $array
+     * @return ezpRestRequest
+     */
+    static public function __set_state( array $array )
+    {
+        return new ezpRestRequest( $array['date'], $array['protocol'],
+            $array['host'], $array['uri'], $array['requestId'],
+            $array['referrer'], $array['variables'], $array['get'],
+            $array['post'], $array['contentVariables'], $array['isEncrypted'],
+            $array['body'], $array['files'], $array['accept'], $array['agent'],
+            $array['authentication'], $array['raw'], $array['cookies'],
+            $array['isFatal'], $array['originalProtocol'], $array['inputVariables'] );
+    }
+
+    /**
+     * Returns base URI with protocol and host (e.g. http://myhost.com/foo/bar)
+     *
+     * @return string
+     */
+    public function getBaseURI()
+    {
+        $hostUri = $this->getHostURI();
+        $apiName = ezpRestPrefixFilterInterface::getApiProviderName();
+        $apiPrefix = eZINI::instance( 'rest.ini' )->variable( 'System', 'ApiPrefix');
+        $uri = str_replace( $apiPrefix, $apiPrefix.'/'.$apiName, $this->uri );
+        $baseUri = $hostUri.$uri;
+
+        return $baseUri;
+    }
+
+    /**
+     * Returns the host with the protocol
+     *
+     * @return string
+     */
+    public function getHostURI()
+    {
+        $protIndex = strpos( $this->protocol, '-' );
+        $protocol = substr( $this->protocol, 0, $protIndex );
+        $hostUri = $protocol.'://'.$this->host;
+
+        return $hostUri;
+    }
+
+    /**
+     * Returns current content variables as a regular query string (e.g. "foo=bar&this=that")
+     *
+     * @param bool $withQuestionMark If true, the question mark ("?") will be added
+     * @return string
+     */
+    public function getContentQueryString( $withQuestionMark = false )
+    {
+        $queryString = '';
+        $aParams = array();
+        foreach( $this->contentVariables as $name => $value )
+        {
+            if( $value !== null )
+                $aParams[] = $name.'='.$value;
+        }
+
+        if( !empty( $aParams ) )
+        {
+            $queryString  = $withQuestionMark ? '?' : '';
+            $queryString .= implode( '&', $aParams );
+        }
+        return $queryString;
+    }
+
+    /**
+     * Get parsed request body based on content type as a php hash.
+     *
+     * In PUT / POST currently only supports application/x-www-form-urlencoded and application/json,
+     * for anything else use ->body atm. If POST then ->post is returned.
+     *
+     * @todo Add some sort of configurable lazy loaded request body handler for parsing misc content type.
+     * @return array|null Null on unsupported protocol or content type.
+     */
+    public function getParsedBody()
+    {
+        return $this->inputVariables;
+    }
+}
+?>
