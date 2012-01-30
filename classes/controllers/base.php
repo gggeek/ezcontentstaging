f10d920a7d9712273207034a982cab592725d8d1
Gaetano Giunta <gg@ez.no>
2012-01-30 17:43:08 +0100

    Try to return an error 500 instead of a php object in case of bad coding serverside

--- classes/controllers/base.php
+++ classes/controllers/base.php
@@ -12,9 +12,25 @@
 
 class eZContentStagingRestBaseController extends ezpRestMvcController
 {
+    /**
+    * Returns an ezpRestMvcResult with the given http code and (error) message
+    * if $message is an object instead of a string, its class name is returned along with 500 internal error
+    */
     protected static function errorResult( $code, $message )
     {
         $result = new ezpRestMvcResult();
+        if ( is_object( $message) )
+        {
+            if ( is_a( $message, 'exception' ) )
+            {
+                $message = $message->getMessage();
+            }
+            else
+            {
+                $code = 500;
+                $message = "Method returned an object: " . get_class( $message );
+            }
+        }
         $result->status = new ezpRestHttpResponse( $code, $message );
         return $result;
     }
